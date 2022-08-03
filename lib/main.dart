0ff7f44eb66bd387949414dc51993a93cce00739
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-08-03 15:20:08 -0300

    Atualização

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,9 @@ class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
+  late Map<String, dynamic> _lastRemoved;
+  late int _lastRemovedPos;
+
   @override
   void initState() {
     super.initState();
@@ -119,7 +122,32 @@ class _HomeState extends State<Home> {
             _saveData();
           });
         },
-      )
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.remove(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+              content: Text("Tarefa \"${_lastRemoved['title']}\" removida!"),
+              action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState((){
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                },
+              ),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
